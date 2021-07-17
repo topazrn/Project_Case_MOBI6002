2be576524ea23a63c0dcaefb693daea4da2e642a
TopazRN <topazsorowako2@gmail.com>
2021-07-17 19:31:37 +0700

    Castle has many Hero and Army

--- app/src/main/java/com/example/project_case_mobi6002/gameplay/castles/Castle.java
+++ app/src/main/java/com/example/project_case_mobi6002/gameplay/castles/Castle.java
@@ -1,5 +1,10 @@
 package com.example.project_case_mobi6002.gameplay.castles;
 
+import com.example.project_case_mobi6002.gameplay.armies.Army;
+import com.example.project_case_mobi6002.gameplay.heroes.Hero;
+
+import java.util.Vector;
+
 public abstract class Castle {
     public static final int STEEL = 0;
     public static final int HORSE = 1;
@@ -9,6 +14,8 @@ public abstract class Castle {
     public static final double[] SKILL_BOOST = {0.2, 0.2, 0.2, 0.2};
 
     private int skin;
+    private Vector<Hero> vHero = new Vector<>();
+    private Vector<Army> vArmy = new Vector<>();
 
     public Castle(int skin) {
         this.skin = skin;
